54ecb98447b788b5c3a580bb9a8fe9ef877181ec
Zelpro <91178393+zelpro-dev@users.noreply.github.com>
2025-03-05 20:20:25 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://zelpro-dev.github.io',
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
